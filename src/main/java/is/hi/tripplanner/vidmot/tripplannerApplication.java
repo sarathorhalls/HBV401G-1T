35481965bbb5ce202f4df2ef72a4b19727ec95e4
BuildTools <unconfigured@null.spigotmc.org>
2022-04-16 20:39:06 +0000

    JavaFX setup with a maven template

--- src/main/java/is/hi/tripplanner/vidmot/tripplannerApplication.java
+++ src/main/java/is/hi/tripplanner/vidmot/tripplannerApplication.java
@@ -1,5 +1,35 @@
 package is.hi.tripplanner.vidmot;
 
-public class tripplannerApplication {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class tripplannerApplication extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("tripplanner-view"), 1280, 720);
+        stage.setTitle("Tripplanner");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(tripplannerApplication.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
